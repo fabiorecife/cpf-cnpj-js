10ac00f9acb07e001d3932157b5a854b8b4fbfb0
fabiorecife <fabiorecife@gmail.com>
2015-05-24 18:05:10 -0300

    add generator

--- js/app.js
+++ js/app.js
@@ -5,21 +5,45 @@
 $("#buttonValidarCPF").click(function() {
     var cpf = $("#cpfvalor").val();
     if (isCPFNumber(cpf)) {
+        $("#alertcpfsuccess").removeClass("in").show();
+        $("#alertcpfsuccess").delay(200).addClass("in").fadeOut(2000);
+        $("#formgroupcpf").addClass("has-success");
+        $("#formgroupcpf").removeClass("has-error");
 
-        $("#alertcpfsuccess").removeClass("sr-only");
-        window.setTimeout(function() {
-            $("#alertcpfsuccess").fadeTo(500, 0).slideUp(500, function(){
-                $(this).addClass("sr-only")
-            });
-        }, 2000);
     } else {
+        $("#alertcpferro").removeClass("in").show();
+        $("#alertcpferro").delay(200).addClass("in").fadeOut(2000);
+        $("#formgroupcpf").removeClass("has-success");
+        $("#formgroupcpf").addClass("has-error");
+    }
+});
 
-        $("#alertcpferro").removeClass("sr-only");
-        window.setTimeout(function() {
-            $("#alertcpferro").fadeTo(500, 0).slideUp(500, function(){
-                $(this).addClass("sr-only")
-            });
-        }, 2000);
+$("#buttonValidarCNPJ").click(function() {
+    var cnpj = $("#cnpjvalor").val();
+    if (isCNPJNumber(cnpj)) {
+        $("#alertcnpjsuccess").removeClass("in").show();
+        $("#alertcnpjsuccess").delay(200).addClass("in").fadeOut(2000);
+        $("#formgroupcnpj").addClass("has-success");
+        $("#formgroupcnpj").removeClass("has-error");
+    } else {
+        $("#alertcnpjerro").removeClass("in").show();
+        $("#alertcnpjerro").delay(200).addClass("in").fadeOut(2000);
+        $("#formgroupcnpj").removeClass("has-success");
+        $("#formgroupcnpj").addClass("has-error");
     }
 
 });
+
+$("#buttonGerarCPF").click(function(){
+   var cpf = generateCPF(true);
+   var lista = $("#listaCPF");
+   var li = $("<li/>").append(cpf).addClass("bg-info").appendTo(lista);
+});
+
+
+$("#buttonGerarCNPJ").click(function(){
+    var cnpj = generateCNPJ(true);
+    var lista = $("#listaCNPJ");
+    var li = $("<li/>").append(cnpj).addClass("bg-warning").appendTo(lista);
+});
+
